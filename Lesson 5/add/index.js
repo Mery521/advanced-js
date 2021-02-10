8426ba3dfa47bf26a3b45a21231b0303a6ec42f6
Mery521 <meryhakobyan879@gmail.com>
2021-02-10 18:24:27 +0400

    Lesson 5

--- Lesson 5/add/index.js
+++ Lesson 5/add/index.js
@@ -1,15 +1,15 @@
 let sum = function (a, b){
     return a + b;
 }
-function square(sum){
+let  square = function(sum){
     return sum * sum;
 }
 
 function f_map(sqr,add){
 
-    function e(){
+   let squareAdd  = function(){
      console.log(sqr(add)+`=`+ add +`^2`);
      }
-     return e;
+     return squareAdd;
 }
  f_map(square, sum(4,5)) (); 
