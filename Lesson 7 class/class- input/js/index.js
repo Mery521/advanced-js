38c97aeeaf6c6dce84c5ab74b4db45f09ad18f35
Mery521 <meryhakobyan879@gmail.com>
2021-03-03 16:28:43 +0400

    Lesson8

--- Lesson 7 class/class- input/js/index.js
+++ Lesson 7 class/class- input/js/index.js
@@ -1,9 +1,10 @@
 
 class Input {
 
-  constructor({ sizes}) {
+  constructor({ sizes, placeholder}) {
     const input = document.createElement("input");
-    
+   input.placeholder = placeholder;
+
     input.classList.add("inp");
     if (sizes in this.sizes) {
       input.classList.add(this.sizes[sizes]);
@@ -11,6 +12,7 @@ class Input {
       console.error(`The ${sizes} isn't in sizes`);
     }
     this.input = input;
+    this.placeholder = pl
     this.sizes;
   }
    
@@ -31,6 +33,7 @@ class Input {
 
 }
 const ToggleMode = new Input({
+  placeholder: 'first',
   sizes: "small",
 });
 const ToggleMode2 = new Input({
---
+++ Lesson 8/index.js
@@ -0,0 +1,13 @@
+
+function multiply(){
+    let input1 = document.getElementById('input1').value;
+    let input2 = document.getElementById('input2').value;
+    document.getElementById("result").innerText= (input1 * input2).toFixed();
+    
+
+}
+function divide(){
+    let input1 = document.getElementById('input1').value;
+    let input2 = document.getElementById('input2').value;
+    document.getElementById("result").innerText= (input1 / input2).toFixed();
+}
